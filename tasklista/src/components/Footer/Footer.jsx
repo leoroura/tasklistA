350478f546109acd3d675d30730d9c5bdb367b52
angie-ekrauth <angela.elizondo2995@gmail.com>
2023-11-21 22:55:21 -0300

    ordené carpetas de componentes, borré lo que sobraba, cambié un poco el css del footer

--- tasklista/src/components/Footer/Footer.jsx
+++ tasklista/src/components/Footer/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
+    <div class="footer">
         <h4>Trabajo Integrativo: Curso "Desarrollo Web con REACT JS - Argentina Programa"</h4>
         <p>Grupo A, integrado por: 
-            <ul>
+            <ul class="lista_alumnos">
                 <li>Diaz, Brian</li>
                 <li>Elizondo, Angela</li>
                 <li>Leonel Roura</li>
